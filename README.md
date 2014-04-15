2bb94d4074eabb836dc0c54bfddf2e951d17142b
Anand Patil <anand@senseplatform.com>
2014-04-14 20:37:56 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,45 @@
+# Dockerflow
+
+Simple ansible-based development flow for [Docker](http://docker.io). Small configuration changes result in quick incremental updates of development images.
+
+## Why?
+
+Existing Docker build systems don't support my preferred development workflow, which has four actions:
+
+- build: Build an image from a base image and a configuration spec. Dockerfiles are fine for this.
+- rebuild: Make a change to the configuration spec; efficiently build an incremental update of the current build.
+- debug-build: Attempt to build an image from the base image and configuration spec, but drop into a shell inside the container on failure so I can identify the problem interactively.
+- debug-rebuild: Like debug-pack, but with repack.
+
+### Why not [Dockerfiles](https://github.com/ansible/ansible)?
+
+Dockerfiles are fine for 'build', but can be inefficient for 'rebuild'. Consider the following Dockerfile:
+
+```
+RUN apt-get install x y z
+RUN long-running-source-build
+...
+```
+
+If you remember that the subsequent steps also require `apt-get install w`, you can either:
+
+- Add `w` to the existing apt-get install line, which means you have to wait for your long-running source build to finish again; or
+- Add a second `apt-get install` line further down, which gets confusing.
+
+Also, I want the images in my repository to correspond to development iterations, not steps in a Dockerfile.
+
+Finally, I like Ansible's declarative approach to configuration management better than Dockerfiles' imperative one.
+
+### Why not [Packer](http://packer.io)?
+
+I don't want the output to be complete tarballs of images, I want to output tagged Docker images that I can push. Also, I want an easy incremental option.
+
+## Usage
+
+`dockerflow action path`, where action can be build, rebuild, debug-build or debug-rebuild.
+
+In more or less the spirit of Dockerfiles, the given path should contain an Ansible playbook called 'dockerflow.yml' which runs the build. In addition, the given path will be available read-only at `/dockerflow` during the build, but will not be bundled into the image.
+
+The playbook must define the vars `dockerflow-base`, which gives the id or tag of the image from which the build should start, and `dockerflow-tag`, which gives the tag that will be applied to the output image.
+
+It may optionally define `dockerflow-docker-options`, which can be an arbitrary string of options to pass to the Docker daemon, and `dockerflow-env`, which is a space-separated list of environmental variables to forward into the container. You can access environmental from playbooks using [this method](http://docs.ansible.com/faq.html#how-do-i-access-shell-environment-variables) or [this method](https://groups.google.com/forum/#!msg/ansible-project/e0erq3FLR5I/vzXm3R8c0BEJ).
---
+++ index.js
@@ -0,0 +1,88 @@
+var fs = require('fs');
+var yaml = require('js-yaml');
+var path = require('path');
+var execSync = require('execSync');
+
+var readConfig = function(buildPath) {
+  var playbookPath = path.join(buildPath, 'dockerflow.yml');
+  if (!fs.existsSync(playbookPath)) {
+    throw new Error("dockerflow.yml not found in " + buildPath);
+  }
+  var playbookYml = yaml.safeLoad(fs.readFileSync(playbookPath, 'utf8'));
+  var vars = playbookYml[0].vars;
+
+  var envKeys = [];
+  if (vars["dockerflow-env"]) {
+    envKeys = vars["dockerflow-env"].split(" ");
+  }
+  var envOpts = [];
+  for (var i = 0; i < envKeys.length; i++) {
+    var k = envKeys[i];
+    envOpts.push("-e " + k + "=$" + k);
+  }
+
+  return {
+    base: vars["dockerflow-base"],
+    tag: vars["dockerflow-tag"],
+    dockerOptions: "-v " + buildPath + ":/dockerflow " + envOpts.join(" ") + " " + (vars["dockerflow-docker-options"] || "")
+  };
+};
+
+var execDebugBuild = function(base, tag, options) {
+  var ctrCmd = " /bin/bash -c \"/bin/bash  --rcfile <(echo 'ansible-playbook /dockerflow/dockerflow.yml -c local -i \\\"127.0.0.1,\\\"')\"";
+  console.log("Dropping you into the debug container. Build will begin immediately.");
+  var runCmd = "docker run -i -t --rm " + options + " " + base + " " + ctrCmd;
+  var cp = execSync.run(runCmd);
+  console.log("Not tagging image in debug mode.");
+};
+
+var execBuild = function(base, tag, options) {
+  var ctrCmd = "ansible-playbook /dockerflow/dockerflow.yml -c local -i \"127.0.0.1,\"";
+  
+  var runCmd = "docker run -i -t -d " + options + " " + base + " " + ctrCmd;
+  var info = execSync.exec(runCmd);
+  if (info.code) {
+    console.log("Unable to start container, code " + info.code + ": " + info.stdout);
+    process.exit();
+  }
+  var ctrId = info.stdout.trim();
+  var cleanup = function() {
+    execSync.run("docker kill " + ctrId);
+    execSync.run("docker rm "+ ctrId);
+    process.exit();
+  };
+  
+  process.on('SIGINT', cleanup);
+  execSync.run("docker logs -f " + ctrId);
+  var codeInfo = execSync.exec("docker wait " + ctrId);
+  if (parseInt(codeInfo.stdout.trim())) {
+    console.log("Build failed, not tagging image.");
+    process.exit();
+  }
+  console.log("Build completed successfully, committing and tagging image.");
+  info = execSync.exec("docker commit " + ctrId);
+  var imgId = info.stdout.trim();
+  execSync.run("docker tag " + imgId + " " + tag);
+  process.removeListener('SIGINT', cleanup);
+  cleanup();
+};
+
+var build = exports.build = function build(buildPath) {
+  var imgs = readConfig(buildPath);
+  execBuild(imgs.base, imgs.tag, imgs.dockerOptions);
+};
+
+var rebuild = exports.rebuild = function rebuild(buildPath) {
+  var imgs = readConfig(buildPath);
+  execBuild(imgs.tag, imgs.tag, imgs.dockerOptions);
+};
+
+var debugBuild = exports["debug-build"] = function debugBuild(buildPath) {
+  var imgs = readConfig(buildPath);
+  execDebugBuild(imgs.base, imgs.tag, imgs.dockerOptions);
+};
+
+var debugRebuild = exports["debug-rebuild"] = function debugRebuild(buildPath) {
+  var imgs = readConfig(buildPath);
+  execDebugBuild(imgs.tag, imgs.tag, imgs.dockerOptions);
+}
